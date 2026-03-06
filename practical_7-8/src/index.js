ccf2ebf9e664ffb887e227321ced5b751dfe6bf3
critical_error <nikula2108@gmail.com>
2026-03-06 18:55:21 +0300

    feat(practical_8): after authorization, a unique JWT token is returned for access to endpoints.

--- practical_7-8/src/index.js
+++ practical_7-8/src/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import { nanoid } from "nanoid";
 import cors from "cors";
+
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
+
+const JWT_SECRET = "access_secret";
+const ACCESS_EXPIRES_IN = "15m";
 
 let products = [
   {
@@ -120,6 +125,29 @@ let products = [
 
 let users = [];
 
+function authMiddleware(req, res, next) {
+  const header = req.headers.authorization || "";
+
+  const [scheme, token] = header.split(" ");
+  if (scheme !== "Bearer" || !token) {
+    return res.status(401).json({
+      error: "Missing or invalid Authorization header",
+    });
+  }
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+
+    // Сохраняем данные токена в req
+    req.user = payload;
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      error: "Invalid or expired token",
+    });
+  }
+}
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -159,7 +187,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${port}`,
+        url: `http://localhost:${PORT}`,
         description: "Локальный сервер",
       },
     ],
@@ -380,7 +408,17 @@ app.post("/api/auth/login", async (req, res) => {
     user.hashedPassword,
   );
   if (isAuthentethicated) {
-    res.status(200).json({ login: true });
+    const accessToken = jwt.sign(
+      {
+        sub: user.id,
+        email: user.email,
+      },
+      JWT_SECRET,
+      {
+        expiresIn: ACCESS_EXPIRES_IN,
+      },
+    );
+    res.status(200).json({ accessToken });
   } else {
     res.status(401).json({ error: "not authentethicated" });
   }
@@ -615,9 +653,9 @@ app.use((err, req, res, next) => {
 });
 
 // Запуск сервера
-app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
   console.log(
-    `Swagger UI доступен по адресу http://localhost:${port}/api-docs`,
+    `Swagger UI доступен по адресу http://localhost:${PORT}/api-docs`,
   );
 });
